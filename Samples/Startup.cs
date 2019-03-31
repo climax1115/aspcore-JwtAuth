7629e9b3d1982a98b30a98e5a6830005fa991994
climax1115 <climax11150102.cmx@gmail.com>
2019-03-31 23:03:11 +0200

    Updated Startup sample.

--- Samples/Startup.cs
+++ Samples/Startup.cs
@@ -1,18 +1,27 @@
 ﻿/*
  * Starup class sample
  * 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using WebApi.Helpers;
-using WebApi.Services;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
-using System.Text;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Mvc;
+using Onesoftdev.AspCoreJwtAuth.Contexts;
+using Onesoftdev.AspCoreJwtAuth.Helpers;
+using Onesoftdev.AspCoreJwtAuth.Services;
 
-namespace WebApi
+namespace Onesoftdev.WebApi1
 {
     public class Startup
     {
@@ -27,6 +36,7 @@ namespace WebApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddCors();
+            services.AddDbContext<UserDbContext>(u => u.UseInMemoryDatabase("TestDb"));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
             // configure strongly typed settings objects
@@ -43,6 +53,21 @@ namespace WebApi
             })
             .AddJwtBearer(x =>
             {
+                x.Events = new JwtBearerEvents
+                {
+                    OnTokenValidated = context =>
+                    {
+                        var userService = context.HttpContext.RequestServices.GetRequiredService<IUserService>();
+                        var userId = Guid.Parse(context.Principal.Identity.Name);
+                        var user = userService.GetUserById(userId);
+                        if (user == null)
+                        {
+                            // return unauthorized if user no longer exists
+                            context.Fail("Unauthorized");
+                        }
+                        return Task.CompletedTask;
+                    }
+                };
                 x.RequireHttpsMetadata = false;
                 x.SaveToken = true;
                 x.TokenValidationParameters = new TokenValidationParameters
@@ -50,9 +75,15 @@ namespace WebApi
                     ValidateIssuerSigningKey = true,
                     IssuerSigningKey = new SymmetricSecurityKey(key),
                     ValidateIssuer = false,
-                    ValidateAudience = false
+                    ValidateAudience = false,
+                    RequireExpirationTime = true,
+                    
+                    // set ClockSkew is zero
+                    ClockSkew = TimeSpan.Zero
                 };
             });
+
+            services.AddScoped<IUserRepository, UserRepository>();
 
             // configure DI for application services
             services.AddScoped<IUserService, UserService>();
@@ -61,6 +92,16 @@ namespace WebApi
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
             // global cors policy
             app.UseCors(x => x
                 .AllowAnyOrigin()
@@ -68,9 +109,11 @@ namespace WebApi
                 .AllowAnyHeader());
 
             app.UseAuthentication();
-            
+
+            app.UseHttpsRedirection();
             app.UseMvc();
         }
     }
 }
+
  */
